79df3ede43c5dc53963134737f7a74cc357d5055
Joe Sweeny <joesweeny84@hotmail.com>
2021-03-29 12:41:54 +0100

    Add deploy scripts

---
+++ .circleci/deploy/docker-push.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION | bash
+
+docker tag "statisticotrader_queue" "$AWS_ECR_ACCOUNT_URL/statistico-trader:$CIRCLE_SHA1"
+docker push "$AWS_ECR_ACCOUNT_URL/statistico-trader:$CIRCLE_SHA1"
