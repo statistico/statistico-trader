098f7fdfd3490166650eda553d88161e1ff22d60
Joe Sweeny <joesweeny84@hotmail.com>
2021-04-05 11:21:39 +0100

    Add classify result parser

--- internal/trader/classify/type.go
+++ internal/trader/classify/type.go
@@ -27,7 +27,29 @@ const (
 	WinLose  = "WIN_LOSE"
 	Lose     = "LOSE"
 	LoseDraw = "LOSE_DRAW"
+
+	Away = "Away"
+	Draw = "Draw"
+	Home = "Home"
+
+	MatchOdds   = "MATCH_ODDS"
+	OverUnder05 = "OVER_UNDER_05"
+	OverUnder15 = "OVER_UNDER_15"
+	OverUnder25 = "OVER_UNDER_25"
+	OverUnder35 = "OVER_UNDER_35"
+	OverUnder45 = "OVER_UNDER_45"
+
+	Fail    = "FAIL"
+	Success = "SUCCESS"
+
+	Back = "BACK"
+	Lay  = "LAY"
+
+	Over  = "Over"
+	Under = "Under"
 )
+
+type Result string
 
 type Fixture struct {
 	ID         uint64
