705742a0400dd732596bc4c0989a7951fb867e45
Joe Sweeny <joesweeny84@hotmail.com>
2021-04-25 18:46:41 +0100

    Add tempoary cors handling

--- cmd/grpc/grpc.go
+++ cmd/grpc/grpc.go
@@ -1,34 +1,54 @@
 package main
 
 import (
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
 	"github.com/statistico/statistico-proto/go"
 	"github.com/statistico/statistico-trader/internal/trader/bootstrap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/keepalive"
 	"google.golang.org/grpc/reflection"
 	"log"
-	"net"
+	"net/http"
 	"time"
 )
 
 func main() {
 	app := bootstrap.BuildContainer(bootstrap.BuildConfig())
-
-	lis, err := net.Listen("tcp", ":50051")
-
-	if err != nil {
-		app.Logger.Fatalf("Failed to listen: %v", err)
-	}
+	logger := app.Logger
 
 	opts := grpc.KeepaliveParams(keepalive.ServerParameters{MaxConnectionIdle:5*time.Minute})
-
 	server := grpc.NewServer(opts)
 
 	statistico.RegisterStrategyServiceServer(server, app.GrpcStrategyService())
-
 	reflection.Register(server)
 
-	if err := server.Serve(lis); err != nil {
+	grpcWebServer := grpcweb.WrapServer(server,
+		grpcweb.WithCorsForRegisteredEndpointsOnly(false),
+		grpcweb.WithOriginFunc(func(origin string) bool {
+			return origin == "https//localhost:3000" || origin == "http://localhost:3000"
+		}),
+	)
+
+	srv := &http.Server{
+		Handler:      http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+			logger.Errorf("Request method %s", req.Method)
+			if grpcWebServer.IsGrpcWebRequest(req) {
+				logger.Error("Request is gRPC")
+				grpcWebServer.ServeHTTP(resp, req)
+				return
+			}
+
+			logger.Error("Request is NOT gRPC")
+			// Fall back to other servers.
+			http.DefaultServeMux.ServeHTTP(resp, req)
+		}),
+		Addr:         "localhost:50051",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  300 * time.Second,
+	}
+
+	if err := srv.ListenAndServe(); err != nil {
+		logger.Errorf("Listen and serve error %+v", err)
 		log.Fatalf("Failed to serve: %v", err)
 	}
 }
--- go.mod
+++ go.mod
@@ -6,15 +6,19 @@ require (
 	github.com/Masterminds/squirrel v1.5.0
 	github.com/aws/aws-sdk-go v1.38.15 // indirect
 	github.com/certifi/gocertifi v0.0.0-20200922220541-2c3bb06c6054 // indirect
+	github.com/desertbit/timer v0.0.0-20180107155436-c41aec40b27f // indirect
 	github.com/evalphobia/logrus_sentry v0.8.2
 	github.com/getsentry/raven-go v0.2.0 // indirect
 	github.com/goccy/go-json v0.4.9 // indirect
 	github.com/golang/protobuf v1.5.2
 	github.com/google/uuid v1.2.0
 	github.com/grpc-ecosystem/go-grpc-middleware v1.2.2
+	github.com/improbable-eng/grpc-web v0.14.0 // indirect
 	github.com/jonboulle/clockwork v0.2.2
+	github.com/klauspost/compress v1.12.1 // indirect
 	github.com/lestrrat-go/jwx v1.1.6
 	github.com/lib/pq v1.10.0
+	github.com/rs/cors v1.7.0 // indirect
 	github.com/sirupsen/logrus v1.7.0
 	github.com/statistico/statistico-betfair-go-client v0.0.0-20210407110526-c811e57a1780
 	github.com/statistico/statistico-data-go-grpc-client v0.0.0-20210317184121-2d206ae02b3e
@@ -22,11 +26,12 @@ require (
 	github.com/statistico/statistico-proto/go v0.0.0-20210422195817-59a711222ee6
 	github.com/stretchr/objx v0.3.0 // indirect
 	github.com/stretchr/testify v1.7.0
-	golang.org/x/net v0.0.0-20210421230115-4e50805a0758 // indirect
-	golang.org/x/sys v0.0.0-20210423082822-04245dca01da // indirect
+	golang.org/x/net v0.0.0-20210423184538-5f58ad60dda6 // indirect
+	golang.org/x/sys v0.0.0-20210423185535-09eb48e85fd7 // indirect
 	google.golang.org/genproto v0.0.0-20210423144448-3a41ef94ed2b // indirect
 	google.golang.org/grpc v1.37.0
 	google.golang.org/grpc/examples v0.0.0-20210331235824-f6bb3972ed15 // indirect
 	google.golang.org/protobuf v1.26.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
 )
