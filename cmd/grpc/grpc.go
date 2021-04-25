9566bde901987d9fabd17a1f7b5a84d46c4c5398
Joe Sweeny <joesweeny84@hotmail.com>
2021-04-25 19:15:04 +0100

    Add temporary grpc service update

--- cmd/grpc/grpc.go
+++ cmd/grpc/grpc.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/improbable-eng/grpc-web/go/grpcweb"
 	"github.com/statistico/statistico-proto/go"
 	"github.com/statistico/statistico-trader/internal/trader/bootstrap"
 	"google.golang.org/grpc"
@@ -8,13 +9,14 @@ import (
 	"google.golang.org/grpc/reflection"
 	"log"
 	"net"
+	"net/http"
 	"time"
 )
 
 func main() {
 	app := bootstrap.BuildContainer(bootstrap.BuildConfig())
 
-	lis, err := net.Listen("tcp", ":50051")
+	lis, err := net.Listen("tcp", ":50052")
 
 	if err != nil {
 		app.Logger.Fatalf("Failed to listen: %v", err)
@@ -28,7 +30,31 @@ func main() {
 
 	reflection.Register(server)
 
-	if err := server.Serve(lis); err != nil {
-		log.Fatalf("Failed to serve: %v", err)
+	go func() {
+		log.Fatal(server.Serve(lis))
+	}()
+
+	grpcWebServer := grpcweb.WrapServer(
+		server,
+		grpcweb.WithCorsForRegisteredEndpointsOnly(false),
+		grpcweb.WithOriginFunc(func(origin string) bool {
+			return origin == "http://localhost:3000" || origin == "http://localhost:3000/"
+		}),
+	)
+
+	srv := &http.Server{
+		Addr:         "localhost:50051",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
 	}
+
+	srv.Handler = http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+		if grpcWebServer.IsGrpcWebRequest(req) {
+			grpcWebServer.ServeHTTP(resp, req)
+		}
+		// Fall back to other servers.
+		http.DefaultServeMux.ServeHTTP(resp, req)
+	})
+
+	log.Fatal(srv.ListenAndServe())
 }
